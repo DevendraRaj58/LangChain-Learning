c9ec74cc89fdb6db242b99a2c22adab0e804f91e
Devendra <devendrarajpurohit58@gmail.com>
2026-07-09 16:14:20 +0530

    Understanding Chains in Langchain

---
+++ Chains/simple_chain.py
@@ -0,0 +1,28 @@
+from langchain_groq import ChatGroq 
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+
+model= ChatGroq(
+    model='llama-3.3-70b-versatile',temperature=0.2
+    )
+
+
+prompt = PromptTemplate(
+template='Generate 5 interesting facts about {topic}',
+input_variables=['topic']
+)
+
+
+parser = StrOutputParser()
+
+chain= prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+chain.get_graph().print_ascii()
